35887f1cbf70098f94acfadc201699664140f04a
OneshotGH <32165081+OneshotGH@users.noreply.github.com>
2017-09-22 11:18:14 +0200

    Revert "Add files via upload"

--- OneshotsGlowHack-master/OneshotsGlowHack/OneshotsGlowHack.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-/*here we have all our includes that we need to include 
-into OneshotsGlowHack.cpp this is nice to do*/
-#include <Windows.h>
-#include "Hprocess.h"
-#include "Glow.h"
-#include <iostream>
